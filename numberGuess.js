946a593cd3687a6b6bcdecd5271e6cca73edb4f4
Kaye Janine Remigio <remigiokayejanine@gmail.com>
2024-10-09 13:39:13 +0800

    Add numberGuess.js and index.html

--- numberGuess.js
+++ numberGuess.js
@@ -1,24 +1,22 @@
 // Program that asks the user to guess a number between 1 and 10.
 
-// Select a random number between 1-10.
-const correctNumber = Math.floor(Math.random() * 10) + 1; 
-
 // Prompt the user for a guess function.
 function guessNumber() {
-  let userGuess;
-  while (userGuess !== correctNumber) {
-    userGuess = parseInt(prompt("Guess a number between 1-10:"));
-    
-    // To check if the user guess is lower, higher, or correct.
-    if (userGuess < correctNumber) {
-         alert(userGuess + " is too low!");
+    // Generate a random number.
+    let correctNumber = Math.floor(Math.random() * 10) + 1; 
+    let userGuess;
+    while (userGuess !== correctNumber) {
+        userGuess = parseInt(prompt("Guess a number between 1-10:"));
+        
+        // To check if the user guess is lower, higher, or correct.
+        if (userGuess < correctNumber) {
+            alert(userGuess + " is too low!");
         } else if (userGuess > correctNumber) {
             alert(userGuess + " is too high!");
         } else if (userGuess === correctNumber) {
-            alert(userGuess + " is correct!");
+            alert(userGuess + " is the correct number!");
         } else {
             alert("Please enter a valid number.");
-        }
+        }    
     }
 }
-guessNumber();
